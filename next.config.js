ebede3a47ee615aefbab89c26cd2e0cf12aab62a
Make-Development <phutthithon@gmail.com>
2020-08-07 22:20:31 +0700

    ssss

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    basePath: '/src',
     env: {
         MONGO_URI: "mongodb+srv://make-develop:makedevelop@cluster0.ahrvn.gcp.mongodb.net/tests?retryWrites=true&w=majority"
     }
