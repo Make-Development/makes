f046d4f9c287d0c747d5dafc6f544e586ff3cc0f
Make-Development <phutthithon@gmail.com>
2020-08-07 22:25:31 +0700

    ssss

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@ module.exports = {
     basePath: '/src',
     env: {
         MONGO_URI: "mongodb+srv://make-develop:makedevelop@cluster0.ahrvn.gcp.mongodb.net/tests?retryWrites=true&w=majority"
-    }
+    },
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+        mySecret: 'secret',
+      },
+      publicRuntimeConfig: {
+        // Will be available on both server and client
+        staticFolder: '/static',
+      }
 }
